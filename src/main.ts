e5d5e45dec08e114e7fb04122e828a4d7eeedfb8
mrLuisFer <mrluisfeer@gmail.com>
2022-08-07 01:19:06 -0500

    add onchange event for the card

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import "./scss/main.scss";
 
-const inputMonth = document.getElementById("month")!;
-const inputYear = document.getElementById("year")!;
-const inputCvc = document.getElementById("cvc")!;
-const fieldName = document.getElementById("field-name")!;
-const fieldNumber = document.getElementById("field-number")!;
+const getElementById = (id: string): HTMLElement => {
+  const element = document.getElementById(`${id}`)!;
+  return element;
+};
+
+const inputMonth = getElementById("month");
+const inputYear = getElementById("year");
+const inputCvc = getElementById("cvc");
+const fieldName = getElementById("field-name");
+const fieldNumber = getElementById("field-number");
 
 const w: string = "50px";
 inputMonth.style.width = w;
@@ -13,3 +18,19 @@ inputYear.style.width = w;
 inputCvc.style.width = "150px";
 fieldName.style.width = "100%";
 fieldNumber.style.width = "100%";
+
+const cardNumberInput: HTMLInputElement | any = getElementById("cardnumber");
+const cardNumber = getElementById("card-number");
+
+cardNumberInput.addEventListener("change", () => {
+  const value = cardNumberInput.value || "0000 0000 0000 0000";
+  cardNumber.textContent = value;
+});
+
+const cardNameInput = getElementById("cardholder")
+const cardName = getElementById("card-name")
+
+cardNameInput.addEventListener("change", () => {
+  const value = cardNameInput.value || "jane appleseed"
+  cardName.textContent = value
+})
